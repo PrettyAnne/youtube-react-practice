98563939c03bb372d637f8597717efd66085d23e
jjh <goodgirl0118@naver.com>
2021-11-02 21:41:05 +0900

    🍒 Search function add

--- src/app.jsx
+++ src/app.jsx
@@ -1,28 +1,26 @@
-import { useState, useEffect } from 'react';
-import styles from './app.module.css';
-import SearchHeader from './components/search_header/search_header';
-import VideoList from './components/video_list/video_list';
+import React, { useState, useEffect } from "react";
+import styles from "./app.module.css";
+import SearchHeader from "./components/search_header/search_header";
+import VideoList from "./components/video_list/video_list";
 
-function App() {
+function App({ youtube }) {
   const [videos, setVideos] = useState([]);
+  const search = (query) => {
+    youtube
+      .search(query) //
+      .then(videos => setVideos(videos));
+  };
 
   useEffect(() => {
-    const requestOptions = {
-      method: 'GET',
-      redirect: 'follow'
-      // redirect: 리퀘스트를 할때 옵션을 전달
-    };
+    youtube
+      .mostPopular() //
+      .then(videos => setVideos(videos));
+  },[]);
 
-    fetch("https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyAoVuULBbNqze5Jhu-ni8DEXtFV8pXTpqQ", requestOptions)
-      .then(response => response.json())
-      // text 보다는 json으로 변환하는게 좋음
-      .then(result => setVideos(result.items))
-      .catch(error => console.log('error', error));
-  }, []);
   return (
     <div className={styles.app}>
-    <SearchHeader />
-    <VideoList videos={videos} />
+      <SearchHeader onSearch={search} />
+      <VideoList videos={videos} />
     </div>
   );
 }
