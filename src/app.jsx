e27fab4a46694d7a66f0bd55421468d7ad7dc4b5
jjh <goodgirl0118@naver.com>
2021-11-02 17:16:37 +0900

    🧁 Create search component, CSS add

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
-import './app.css';
+import styles from './app.module.css';
+import SearchHeader from './components/search_header/search_header';
 import VideoList from './components/video_list/video_list';
 
 function App() {
@@ -19,7 +20,10 @@ function App() {
       .catch(error => console.log('error', error));
   }, []);
   return (
+    <div className={styles.app}>
+    <SearchHeader />
     <VideoList videos={videos} />
+    </div>
   );
 }
 
